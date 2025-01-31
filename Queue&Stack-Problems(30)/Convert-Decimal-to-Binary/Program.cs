a431c7eaa4b9499fa65dc211db375cd75be167ac
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2025-01-31 23:32:31 +0200

    Add files via upload

---
+++ Queue&Stack-Problems(30)/Convert-Decimal-to-Binary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_Solving
+{
+    /*         
+               ------------Problem-------------
+              
+        Convert a decimal number to binary using a stack.
+
+     */
+
+    public class Program
+    {
+        public static string toBinary(int input)
+        {
+            if (input == 0) return "0";
+
+            var stack = new Stack<char>();
+            while (input > 0)
+            {
+                stack.Push((input % 2 == 0) ? '0' : '1');
+                input /= 2;
+            }
+            return toString(stack);
+        }
+        public static string toString(Stack<char> stack)
+        {
+            var result = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                result.Append( stack.Pop());
+            }
+            return result.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(toBinary(24));
+        }
+    }
+}
