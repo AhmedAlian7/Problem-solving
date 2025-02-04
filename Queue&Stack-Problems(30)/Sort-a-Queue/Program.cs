39984603d1ccd6e46612a50b92d7f143d0410971
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2025-02-04 20:35:24 +0200

    Add files via upload

---
+++ Queue&Stack-Problems(30)/Sort-a-Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_Solving
+{
+    /*         
+                      ------------Problem-------------
+
+            Sort a Queue
+            Problem: Sort elements in a queue in ascending order.
+            
+            Example:
+            Input: Queue = [5, 1, 3, 2, 4]
+            Output: Queue = [1, 2, 3, 4, 5]
+
+     */
+
+    public class Program
+    {
+
+        static Queue<int> SortQueue(Queue<int> q)
+        {
+            var list  = new List<int>(q);
+            list.Sort();
+
+            return new Queue<int>(list);
+        }
+
+        static void Main(string[] args)
+        {
+            var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            
+            var queue = SortQueue(new Queue<int>(arr));
+            Console.WriteLine(string.Join(" ", queue));
+        }
+    }
+}
