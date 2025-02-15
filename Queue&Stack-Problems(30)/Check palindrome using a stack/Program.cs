596678b337e2f967286f117a985cc3336fe9f46a
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2025-02-15 22:38:09 +0200

    Add files via upload

---
+++ Queue&Stack-Problems(30)/Check palindrome using a stack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_Solving
+{
+    /*         
+                            ------------Problem-------------
+
+                Check palindrome using a stack 
+                Problem:
+                Check if a given string is a palindrome using a stack.
+                
+                - Input: "madam"
+                - Output: true
+
+                - Input: "hello"
+                - Output: false
+    */
+
+    public class Program
+    {
+        
+        public static bool isPalindrome(string s)
+        {
+            var stack = new Stack<char>(s);
+
+            foreach (var c in s)
+            {
+                if (c != stack.Pop())
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine().Trim();
+
+            Console.Write(isPalindrome(s));
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
