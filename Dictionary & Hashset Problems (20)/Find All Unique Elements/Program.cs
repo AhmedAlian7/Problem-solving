f4010b7cb74d04a60b03a7f4547b6734bb213470
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2025-02-28 14:29:22 +0200

    Add files via upload

---
+++ Dictionary & Hashset Problems (20)/Find All Unique Elements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_Solving
+{
+
+    /*
+                    -----------------Problem-----------------
+     
+                Find All Unique Elements
+                Problem: Return all unique elements from an array.
+                
+                Example:
+                - Input: [1, 2, 2, 3, 4, 5, 3]
+                - Output: [1, 4, 5]
+
+     */
+
+    public class Progam
+    {
+        public static IList<int> FindUniques(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            var res = new List<int>();
+            foreach (int num in nums)
+            {
+                if (dic.ContainsKey(num))
+                    dic[num]++;
+                else
+                    dic.Add(num, 1);
+                
+
+            }
+
+            foreach (var item in dic)
+            {
+                if (item.Value == 1)
+                    res.Add(item.Key);
+            }
+            return res;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            var res = string.Join(",", FindUniques([1, 2, 2, 3, 4, 5, 3]));
+            Console.WriteLine($"[{res}]");
+        }
+    }
+}
+
+
+
+
