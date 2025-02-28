34826ece30d588265c47293033b716ef274520fa
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2025-02-28 14:13:46 +0200

    Add files via upload

---
+++ Dictionary & Hashset Problems (20)/Majority-Element/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_Solving
+{
+
+    /*
+                            -----------------Problem-----------------
+     
+                Problem: Find the majority element in an array (element appearing more than n/2 times).
+
+                Example:
+                - Input: [3, 2, 3]
+                - Output: 3
+
+     */
+
+    public class Progam
+    {
+        public static int majorityElement(int[] nums)
+        {
+            int n = nums.Length;
+            var dic = new Dictionary<int, int>();
+
+            foreach ( var item in nums)
+            {
+                if(dic.ContainsKey(item))
+                {
+                    dic[item]++;
+                }
+                else
+                {
+                    dic.Add(item, 1);
+                }
+                if (dic[item] > n / 2)
+                {
+                    return item;
+                }
+            }
+            return -1;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(majorityElement([3, 2, 3]));
+        }
+    }
+}
+
+
+
+
