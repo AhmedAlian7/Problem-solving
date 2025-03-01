0a9161606eec708261c88ea7820746b1af00d05c
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2025-03-01 13:59:26 +0200

    Add files via upload

---
+++ Dictionary & Hashset Problems (20)/missing-number/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_Solving
+{
+
+    /*
+                            -----------------Problem-----------------
+     
+                Find Missing Number in an Array
+                Problem: Find the missing number in an array of size n containing numbers from 0 to n.
+                
+                Example:
+                - Input: [3, 0, 1]
+                - Output: 2
+
+     */
+
+
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+
+            var set = new HashSet<int>(nums);
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (!set.Contains(i)) return i;
+            }
+            return -1;
+        }
+    }
+
+
+    public class Progam
+    {
+       
+        public static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.MissingNumber([3, 0, 1]));
+        }
+    }
+}
+
+
+
+
